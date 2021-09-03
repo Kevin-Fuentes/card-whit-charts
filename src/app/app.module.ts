67a4343b7fbe3fdeab19ebab58d66b21a54fc2e0
Kevin F <kfuentesm2@unicartagena.edu.co>
2021-09-03 15:10:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HighchartsChartModule } from 'highcharts-angular';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
+import { DashboardsComponent } from './components/smarts/dashboards/dashboards.component';
+import { MetricChartsComponent } from './components/dummies/metric-charts/metric-charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardsComponent,
+    MetricChartsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HighchartsChartModule,
+    MatCardModule,
+    MatIconModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
